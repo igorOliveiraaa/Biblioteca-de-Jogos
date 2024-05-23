01a5b787ef15c4b04badbfa3a746a9ed5879af5c
Igor <igor.oliveira@easyworkoutsourcing.com>
2024-05-23 10:42:30 -0300

    correction

--- Biblioteca de Jogos/src/App.jsx
+++ Biblioteca de Jogos/src/App.jsx
@@ -3,21 +3,24 @@ import NewGameForm from "./components/NewGameForm";
 import useGameCollection from "./hooks/useGameCollection";
 
 export default function App() {
-  const {games, addGame, removeGame} = useGameCollection()
+  const { games, addGame, removeGame } = useGameCollection();
 
   return (
     <div id="app">
       <h1>Biblioteca de Jogos</h1>
       <NewGameForm addGame={addGame} />
       <div className="games">
-        {games.map((game) => (
+        {games.length > 0 
+        ? games.map((game) => (
           <Game
             key={game.id}
             title={game.title}
             cover={game.cover}
             onRemove={() => removeGame(game.id)}
           />
-        ))}
+        )) : (
+          <h2 style={{margin: "4rem auto", alignItems:"center"}}>Adicione um Jogo</h2>
+        )}
       </div>
     </div>
   );
